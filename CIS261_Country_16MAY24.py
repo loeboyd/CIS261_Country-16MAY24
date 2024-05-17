378d5c676f27d13ba00e4c7f9d7b2b6a99fbbbf1
username <useremail>
2024-05-16 19:54:56 -0700

    Add project files.

---
+++ CIS261_Country_16MAY24.py
@@ -0,0 +1,72 @@
+#Lois Daniels
+#CIS261
+#16MAY24
+
+def display_menu():
+    print("COMMAND MENU")
+    print("view - View country name")
+    print("add - Add a country")
+    print("del - Delete a country")
+    print("exit - Exit program")
+    print()
+    
+def display_codes(countries):
+    codes = list(countries.keys())
+    codes.sort()
+    codes_line = "Country codes: "
+    for code in codes:
+        codes_line += code + " "
+    print(codes_line)
+    
+def view(countries):
+    display_codes(countries)
+    code = input("Enter country code: ")
+    code = code.upper()
+    if code in countries:
+       name = countries[code]
+       print(f"Country name: {name}.\n")
+    else:
+        print("There is no country with that code.\n")
+        
+def add(countries):
+    code = input("Enter country code: ")
+    code = code.upper()
+    if code in countries:
+        name = countries[code]
+        print(f"{name} is already using this code.\n")
+    else:
+       name = input("Enter country name: ")
+       name = name.title()
+       countries[code] = name
+       print(f"{name} was added.\n")
+       
+def delete(countries):
+    code = input("Enter country code: ")
+    code = code.upper()
+    if code in countries:
+        name = countries.pop(code)
+        print(f"{name} was deleted.\n")
+    else:
+        print("There is no country with that code.\n")
+        
+def main():
+    countries = {"IN": "India", "GH": "Ghana", "US": "United States", "CA": "Canada"}
+    
+    display_menu()
+    while True:
+        command = input("Command: ")
+        command = command.lower()
+        if command == "view":
+            view(countries)
+        elif command == "add":
+            add(countries)
+        elif command == "del":
+            delete(countries)
+        elif command == "exit":
+            print("Good Bye!")
+            break
+        else:
+            print("Not a valid command. Please try again.\n")
+            
+if __name__ == "--main__":
+    main()
